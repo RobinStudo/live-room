8c505309c4c88750158fd5d0580f593a8a92e911
Robin Delbaere <hello@rdelbaere.fr>
2022-07-28 14:48:06 +0200

    Reorganize front socket code

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
-import { io } from "https://cdn.socket.io/4.4.1/socket.io.esm.min.js";
+import { Room } from './src/Room.js';
 
-const socket = io('ws://localhost:3000');
-const elements = {
-    feed: document.getElementById('feed'),
-    box: document.getElementById('box'),
-    boxInput: document.querySelector('#box input'),
-};
-
-elements.box.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const message = elements.boxInput.value;
-    if(message.length === 0){
-        return;
-    }
-
-    socket.emit('originMessage', message);
-    elements.boxInput.value = '';
-
-    const messageElement = document.createElement('article');
-    messageElement.classList.add('message');
-    messageElement.classList.add('sent');
-    messageElement.innerText = message;
-
-    elements.feed.prepend(messageElement);
-});
-
-socket.on('forwardMessage', (message) => {
-    const messageElement = document.createElement('article');
-    messageElement.classList.add('message');
-    messageElement.classList.add('recived');
-    messageElement.innerText = message;
-
-    elements.feed.prepend(messageElement);
-});
+new Room();
